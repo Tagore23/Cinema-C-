51713e93ac16bc44d87aae9cb96ca0c4f0e26f15
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-06-29 10:40:22 -0300

    styled-components restabelecido

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import FilmeListar from "./components/pages/Filme/Filme-listar";
-import SessaoCadastrar from "./components/pages/sessao/sessao-cadastrar";
+import styled from "styled-components";
+import SessaoCadastrar from "./components/pages/Filme/sessao/sessao-cadastrar";
 
 function App() {
   return (
